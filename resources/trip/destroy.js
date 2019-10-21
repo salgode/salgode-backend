b95188778cced1dc4c5e0922ebfd32d3295ce67d
Bernardo Subercaseaux <bernardosubercaseaux@gmail.com>
2019-10-21 01:29:57 -0300

    feat: add trips endpoints (#4)
    Features
      Add initial trip endpoints

---
+++ resources/trip/destroy.js
@@ -0,0 +1,26 @@
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const { InternalServerError } = require('../../constants/validationResponses');
+
+module.exports = function(event, callback) {
+  console.log('enters destroy');
+  const params = {
+    TableName: event.TableName,
+    Key: event.Key
+  };
+
+  console.log('params\n', params);
+
+  return dynamoDb.delete(params, error => {
+    if (error) {
+      console.error(error);
+      return callback(null, InternalServerError);
+    }
+
+    const response = {
+      statusCode: 200
+    };
+    return callback(null, response);
+  });
+};
