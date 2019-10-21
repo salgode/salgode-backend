8df1711a9114632ab672ad8f69dac8cdd8c3170e
brunocalderon <b@brunocalderon.com>
2019-10-21 07:36:45 -0300

    fix(slots): create slot
    The slot can be created only if there are available seats on the trip

--- create_slot.js
+++ create_slot.js
@@ -6,45 +6,48 @@ const moment = require('moment');
 async function createSlot(tripId, userId) {
   let slotId = 'slo_'+uuidv4();
   let createdAt = moment().format('YYYY-MM-DDTHH:mm:ss-04:00');
-  let data = await dynamoDB.transactWrite({
-    TransactItems: [
-        {
-            Put: {
-                TableName: process.env.dynamodb_table_name_slots,
-                Item: {
-                    "slot_id": slotId,
-                    "trip_id": tripId,
-                    "user_id": userId,
-                    "slot_status": "requested",
-                    "created_at": createdAt
-                }
-            }
-        },
-        {
-            Update: {
-                TableName: process.env.dynamodb_table_name_trips,
-                Key: {
-                    "trip_id": tripId
-                },
-                UpdateExpression: "set available_seats = available_seats - :requested_seats",
-                ExpressionAttributeValues: {
-                    ":requested_seats": 1
-                }
-            }
-        }
-    ]
-  }).promise();
-  return data;
+  try {
+    await dynamoDB.transactWrite({
+      TransactItems: [
+          {
+              ConditionCheck: {
+                  TableName: process.env.dynamodb_table_name_trips,
+                  Key: {
+                      "trip_id": tripId
+                  },
+                  ConditionExpression: "available_seats >= :available_seats",
+                  ExpressionAttributeValues: {
+                      ":available_seats": 1,
+                  }
+              }
+          },
+          {
+              Put: {
+                  TableName: process.env.dynamodb_table_name_slots,
+                  Item: {
+                      "slot_id": slotId,
+                      "trip_id": tripId,
+                      "user_id": userId,
+                      "slot_status": "requested",
+                      "created_at": createdAt
+                  }
+              }
+          }
+      ]
+    }).promise();
+    return true;
+  }
+  catch(e) {
+    return false;
+  }
 }
 
 exports.handler = async (event) => {
   let tripId = event.trip_id;
   let userId = event.user_id;
 
-  await createSlot(tripId, userId);
-
   let result = {
-    created: true
+    created: await createSlot(tripId, userId)
   };
 
   return result;
