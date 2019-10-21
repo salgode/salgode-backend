e6d84c6ddfabc8b6e60ebc82e52654e20fb78b17
cristianeing <cristian.eing@gmail.com>
2019-10-21 16:30:50 -0300

    Add get trip next stop

--- getTripNextStop.js
+++ getTripNextStop.js
@@ -2,7 +2,9 @@ const AWS = require('aws-sdk');
 var dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = function(event, context, callback) {
-  var tripId = event.tripId; 
+  // var tripId = event.tripId;
+  var tripId = event.pathParameters.trip;
+  console.log({ tripId });
   var getParams = {
     TableName: 'SalgoDe_Trips',
     Key: {
@@ -28,32 +30,33 @@ module.exports.handler = function(event, context, callback) {
     //   return callback(null, Unauthorized);
     // }
     console.log(getData.Item);
-    if(getData.Item.trip_status !== 'in_progress') {
-        return callback(null, invalidState);
+    if (getData.Item.trip_status !== 'in_progress') {
+      return callback(null, invalidState);
     }
-    
+
     var index = getData.Item.next_stop || 0;
     var spot_id = getData.Item.route_points[index];
     console.log('VAMOS VAMOS', spot_id);
-    if(!spot_id) {
+    if (!spot_id) {
       return callback(null, noSpotToGo);
     }
 
-      var getSpotParam = {
-        TableName: 'SalgodeSpot',
-        Key: {
-          id: spot_id
-        }
-     };
+    var getSpotParam = {
+      TableName: 'SalgodeSpot',
+      Key: {
+        id: spot_id
+      }
+    };
 
     return dynamoDb.get(getSpotParam, (error, spot) => {
       if (error) {
         console.error(error);
         return callback(null, InternalServerError);
       }
+      const body = { next_stop_index: index, stop: spot.Item };
       var response = {
         statusCode: 200,
-        spot: spot.Item
+        body: JSON.stringify(body)
       };
       return callback(null, response);
     });
