020f34a46eec20314d5242a1cd7e265e024201ce
brunocalderon <b@brunocalderon.com>
2019-10-22 19:37:28 -0300

    chore(eslint): airbnb-base
    Eslinting with modified Airbnb Base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,20 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true
-    },
-    extends: [
-        "prettier"
-    ],
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
-    },
-    parserOptions: {
-        ecmaVersion: 2018
-    },
-    plugins: [
-      "prettier"
-    ],
-    rules: {
-      "prettier/prettier": ["error", {
-      trailingComma: "none",
-      semi: true,
-      singleQuote: true,
-    }]
-    }
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'airbnb-base'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'comma-dangle': ['error', 'never']
+  }
 };
