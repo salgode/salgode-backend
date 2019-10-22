61fb889e3f30a513334c034aeb1f094e7a90f8e8
brunocalderon <b@brunocalderon.com>
2019-10-22 17:06:17 -0300

    refactor(get_trip): refactored code

--- get_trip.js
+++ get_trip.js
@@ -1,76 +1,50 @@
 const aws = require('aws-sdk');
+
 const dynamoDB = new aws.DynamoDB.DocumentClient();
 
 async function getTrip(tripId) {
-  let params = {
-    TableName: 'SalgoDe_Trips',
-    ScanIndexForward: true,
-    ProjectionExpression:
-      '#trip_id, #created_at, #route_points, #trip_status, #etd, #car_id, #driver_id, #next_stop, #available_seats, #updatedAt',
-    ExpressionAttributeNames: {
-      '#trip_id': 'trip_id',
-      '#created_at': 'created_at',
-      '#route_points': 'route_points',
-      '#trip_status': 'trip_status',
-      '#etd': 'etd',
-      '#car_id': 'car_id',
-      '#driver_id': 'driver_id',
-      '#next_stop': 'next_stop',
-      '#available_seats': 'available_seats',
-      '#updatedAt': 'updatedAt'
+  const params = {
+    TableName: process.env.dynamodb_table_name_trip,
+    Key: {
+      trip_id: tripId,
     },
-    KeyConditionExpression: 'trip_id = :trip_id',
-    ExpressionAttributeValues: {
-      ':trip_id': tripId
-    }
+    ProjectionExpression: 'trip_id, created_at, route_points, trip_status, etd',
   };
-  let data = await dynamoDB.query(params).promise();
-  return data.Items;
+  const data = await dynamoDB.get(params).promise();
+  return data.Item;
 }
 
-async function getSpot(spotId) {
-  let params = {
-    TableName: 'SalgodeSpot',
-    ScanIndexForward: true,
-    ProjectionExpression:
-      '#id, #address, #city, #commune, #icon, #lat, #lon, #name, #type',
+// Same code as on get_spot.js 4:1
+async function getSpot(spotID) {
+  const params = {
+    TableName: process.env.dynamodb_table_name_spot,
+    Key: {
+      id: spotID,
+    },
+    ProjectionExpression: 'spot_id, address, city, commune, icon, lat, lon, #name, #type',
     ExpressionAttributeNames: {
-      '#id': 'id',
-      '#address': 'address',
-      '#city': 'city',
-      '#commune': 'commune',
-      '#icon': 'icon',
-      '#lat': 'lat',
-      '#lon': 'lon',
       '#name': 'name',
-      '#type': 'type'
+      '#type': 'type',
     },
-    KeyConditionExpression: 'id = :id',
-    ExpressionAttributeValues: {
-      ':id': spotId
-    }
   };
-  let data = await dynamoDB.query(params).promise();
-  return data.Items[0];
+  const data = await dynamoDB.get(params).promise();
+  return data.Item;
 }
 
-async function getSpotAsync(route_points) {
-  const promises = route_points.map(async pointId => {
-    console.log(pointId);
-    return await getSpot(pointId);
-  });
-  return Promise.all(promises);
+async function translateSpotId(spotsArray) {
+  return await Promise.all(
+    spotsArray.map(async (pointId) => await getSpot(pointId)),
+  );
 }
 
-exports.handler = async event => {
-  let tripId = event.pathParameters.trip;
-  let result = await getTrip(tripId);
-  let routePoints = await getSpotAsync(result[0].route_points);
-  result[0].route_points = routePoints;
+exports.handler = async (event) => {
+  const tripId = event.pathParameters.trip;
+  const tripResult = await getTrip(tripId);
+  tripResult.trip_route_points = await translateSpotId(tripResult.route_points);
   const response = {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(result[0])
+    body: JSON.stringify(tripResult),
   };
   return response;
 };
