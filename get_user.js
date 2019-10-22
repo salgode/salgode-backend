83fa1c454478474622fc7d3bf6e38e37fb17cd74
brunocalderon <b@brunocalderon.com>
2019-10-22 14:24:13 -0300

    chore(get_user): refactored code

--- get_user.js
+++ get_user.js
@@ -3,36 +3,34 @@ const dynamoDB = new aws.DynamoDB.DocumentClient();
 
 async function getUser(userId) {
   let params = {
-    TableName: process.env.dynamodb_table_name,
-    ScanIndexForward: true,
-    ProjectionExpression:
-      '#user_id, #email, #first_name, #last_name, #phone, #user_identifications, #car',
-    ExpressionAttributeNames: {
-      '#user_id': 'user_id',
-      '#email': 'email',
-      '#first_name': 'first_name',
-      '#last_name': 'last_name',
-      '#phone': 'phone',
-      '#user_identifications': 'user_identifications',
-      '#car': 'car'
-    },
-    KeyConditionExpression: 'user_id = :user_id',
-    ExpressionAttributeValues: {
-      ':user_id': userId
-    }
+      TableName : process.env.dynamodb_table_name,
+      Key: {
+          "user_id": userId
+      },
+      ProjectionExpression: "user_id, email, first_name, last_name, phone, user_identifications, car"
   };
-  let data = await dynamoDB.query(params).promise();
-  return data.Items[0];
+  let data = await dynamoDB.get(params).promise();
+  return data.Item;
 }
 
 exports.handler = async event => {
   let userId = event.pathParameters.id;
-  console.log('userId', userId);
   let result = await getUser(userId);
-  const response = {
-    statusCode: 200,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(result)
-  };
-  return response;
+  if (result) {
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(result)
+    };
+  }
+  else {
+    let responseBody = {
+      message: 'User does not exist'
+    };
+    return {
+      statusCode: 422,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(responseBody)
+    };
+  }
 };
