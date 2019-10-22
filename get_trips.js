6369d5d72ba2f91b14e7d516b5b58b6aa8fb830b
brunocalderon <b@brunocalderon.com>
2019-10-22 20:49:51 -0300

    fix(get_trips): available trips only
    Now the result is filtered to show only open and in_progress trips

--- get_trips.js
+++ get_trips.js
@@ -1,16 +1,21 @@
 const { parallelScan } = require('@shelf/dynamodb-parallel-scan');
 
 async function getTrips() {
-  let params = {
+  const params = {
     TableName: process.env.dynamodb_table_name,
-    ProjectionExpression: 'available_seats, driver_id, etd, route_points, trip_id, trip_status'
+    ProjectionExpression: 'available_seats, driver_id, etd, route_points, trip_id, trip_status',
+    FilterExpression: 'trip_status in (:status_1, :status_2)',
+    ExpressionAttributeValues: {
+      ':status_1': 'open',
+      ':status_2': 'in_progress'
+    }
   };
-  let data = await parallelScan(params, { concurrency: 1000 });
+  const data = await parallelScan(params, { concurrency: 1000 });
   return data;
 }
 
-exports.handler = async event => {
-  let allTrips = await getTrips();
+exports.handler = async (event) => { // eslint-disable-line no-unused-vars
+  const allTrips = await getTrips();
   const response = {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
