d86388e4c1aadb0ba2723c480ac5a80979de7ca7
Franco Méndez <fnmendez@uc.cl>
2019-10-23 19:24:02 -0300

    feat(layer): add bearer to user id layer

--- bearer_to_user_id.js
+++ bearer_to_user_id.js
@@ -2,10 +2,10 @@ const aws = require('aws-sdk');
 
 const dynamoDB = new aws.DynamoDB.DocumentClient();
 
-async function getUser(bearerToken) {
+exports.bearerToUserId = async function getUser(bearerToken) {
   const params = {
-    TableName: process.env.dynamodb_table_name,
-    IndexName: process.env.dynamodb_index_name,
+    TableName: 'Users',
+    IndexName: 'bearer_token-index',
     KeyConditionExpression: 'bearer_token = :bearerToken',
     ProjectionExpression: 'user_id',
     ExpressionAttributeValues: {
@@ -14,25 +14,4 @@ async function getUser(bearerToken) {
   };
   const data = await dynamoDB.query(params).promise();
   return data.Items[0].user_id;
-}
-
-exports.handler = async (event) => {
-  const bearerToken = event.headers.Authorization.substring(7);
-  const result = await getUser(bearerToken);
-  if (result) {
-    return {
-      statusCode: 200,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: result
-    };
-  }
-
-  const responseBody = {
-    message: 'User does not exist'
-  };
-  return {
-    statusCode: 404,
-    headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(responseBody)
-  };
 };
