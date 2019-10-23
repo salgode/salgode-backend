dc1e49d7f3b689ffaee5f37e17608be4b39347b7
cristianeing <cristian.eing@gmail.com>
2019-10-23 02:05:27 -0300

    refactor: pass code through es lint

--- get_user_trips.js
+++ get_user_trips.js
@@ -1,32 +1,33 @@
 const AWS = require('aws-sdk');
-var docClient = new AWS.DynamoDB.DocumentClient();
 
-async function getTripAsDriver(user_id) {
-  let params = {
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+async function getTripAsDriver(userId) {
+  const params = {
     TableName: process.env.dynamodb_trips_table_name,
-    IndexName: "driver_id-index",
-    KeyConditionExpression: "#driver = :driver",
+    IndexName: 'driver_id-index',
+    KeyConditionExpression: '#driver = :driver',
     ExpressionAttributeNames: {
-      "#driver": "driver_id"
+      '#driver': 'driver_id'
     },
     ExpressionAttributeValues: {
-      ":driver": user_id
+      ':driver': userId
     }
   };
   const data = await docClient.query(params).promise();
   return data.Items;
 }
 
-async function getTripForUser(user_id) {
-  let params = {
+async function getTripForUser(userId) {
+  const params = {
     TableName: process.env.dynamodb_trips_slot_table_name,
-    IndexName: "user_id-index",
-    KeyConditionExpression: "#user = :user",
+    IndexName: 'user_id-index',
+    KeyConditionExpression: '#user = :user',
     ExpressionAttributeNames: {
-      "#user": "user_id"
+      '#user': 'user_id'
     },
     ExpressionAttributeValues: {
-      ":user": user_id
+      ':user': userId
     }
   };
   const data = await docClient.query(params).promise();
@@ -46,31 +47,29 @@ async function getTrip(tripId) {
 }
 
 async function translateTripId(tripSlotsArray) {
-  return await Promise.all(
-    tripSlotsArray.map(async (tripSlot) => {
-      return await getTrip(tripSlot.trip_id);
-    })
+  return Promise.all(
+    tripSlotsArray.map(async (tripSlot) => getTrip(tripSlot.trip_id))
   );
 }
 
 exports.handler = async (event) => { // eslint-disable-line no-unused-vars
-  const user_id = event.pathParameters.id
+  const userId = event.pathParameters.id;
   const query = event.queryStringParameters && event.queryStringParameters.role ? event.queryStringParameters.role : false;
-  let allDriverTrips = []
-  let allUserTripIds = []
+  let allDriverTrips = [];
+  let allUserTripIds = [];
   switch (query) {
-    case "pax":
-      allUserTripIds = await getTripForUser(user_id);
+    case 'pax':
+      allUserTripIds = await getTripForUser(userId);
       break;
-    case "driver":
-      allDriverTrips = await getTripAsDriver(user_id);
+    case 'driver':
+      allDriverTrips = await getTripAsDriver(userId);
       break;
     default:
-      allDriverTrips = await getTripAsDriver(user_id);
-      allUserTripIds = await getTripForUser(user_id);
+      allDriverTrips = await getTripAsDriver(userId);
+      allUserTripIds = await getTripForUser(userId);
       break;
   }
-  let allTrips = await translateTripId(allUserTripIds)
+  let allTrips = await translateTripId(allUserTripIds);
   allTrips = allTrips.concat(allDriverTrips);
   const response = {
     statusCode: 200,
