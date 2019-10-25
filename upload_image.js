41b9d9b99f48501d28a7e4c9a3a8799814359e6a
brunocalderon <b@brunocalderon.com>
2019-10-24 23:29:47 -0300

    fix(upload_image): fixed the upload to s3
    WIP: get the image id from the database

--- upload_image.js
+++ upload_image.js
@@ -1,78 +1,51 @@
-const AWS = require('aws-sdk');
+const aws = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
 
-const s3 = new AWS.S3({ apiVersion: '2008-10-17' });
-const sha1 = require('sha1');
+const s3 = new aws.S3({
+  apiVersion: '2008-10-17',
+  accessKeyId: process.env.access_key_id,
+  secretAccessKey: process.env.access_key_secret
+});
+const S3_BUCKET = process.env.s3_bucket;
 
-const ValidationErrorInvalidFile = {
-  statusCode: 415,
-  body: JSON.stringify({
-    message: 'Tipo de archivo no valido'
-  }),
-  isBase64Encoded: false
-};
-
-const InternalServerError = {
-  statusCode: 503,
-  body: JSON.stringify({
-    message: 'Algo inesperado acaba de pasar... gracias por intentar más tarde'
-  }),
-  isBase64Encoded: false
-};
-
-const getFile = function getFile(fileMime, buffer) {
-  const fileExt = fileMime;
-  const hash = sha1(Buffer.from(Date().toString()));
-  const now = Date.now();
-
-  const filePath = `${hash}/`;
-  const fileName = `${now}.${fileExt}`;
-  const fileFullName = filePath + fileName;
-  const fileFullPath = `https://salgode-bucket.s3.amazonaws.com/${fileFullName}`;
-
-  const params = {
-    Key: fileFullName,
-    Bucket: 'salgode-bucket',
-    Body: buffer
-  };
-
-  const uploadFile = {
-    size: buffer.toString('ascii').length,
-    type: `image/${fileExt}`,
-    name: fileName,
-    full_path: fileFullPath
-  };
-
-  return {
-    params,
-    uploadFile
-  };
-};
-
-module.exports.handler = function (event, context, callback) {
-  const request = JSON.parse(event.body).base64string;
-  const customArgs = request.split(',')[0].split(';')[0] || [];
-  const extension = customArgs.split('/')[1];
-  const base64string = request.split(',')[1];
-  const buffer = Buffer.from(base64string, 'base64');
-  const fileMime = extension;
-
-  if (fileMime === null) {
-    return callback(null, ValidationErrorInvalidFile);
-  }
-
-  const file = getFile(fileMime, buffer);
-  const { params } = file;
-  return s3.upload(params, async (err) => {
-    if (err) {
-      callback(err, InternalServerError);
-    } else {
-      const response = {
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify(file.uploadFile),
-        isBase64Encoded: false
-      };
-      callback(null, response);
-    }
+async function getSignedUrl(s3Params, folderName, fileName) {
+  return new Promise((resolve, reject) => {
+    s3.getSignedUrl('putObject', s3Params, (err, url) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(url);
+      }
+    });
   });
+}
+
+//todo get image id
+async function getImageId(folderName, fileName) {
+  return 'img_12345';
+}
+
+exports.handler = async (event) => {
+  const fileName = uuidv4() + event.file_name;
+  const fileType = event.file_type;
+  const folderName = uuidv4();
+
+  const s3Params = {
+    Bucket: `${S3_BUCKET}/${folder}`,
+    Key: fileName,
+    Expires: 1000,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  const result = {
+    success: true,
+    image_id: await getImageId(folderName, fileName),
+    image_urls: {
+      upload: await getSignedUrl(s3Params, folderName, fileName),
+      fetch: 'https://static.salgode.com/'+folderName+'/'+fileName
+    }
+  };
+
+  return result;
 };
