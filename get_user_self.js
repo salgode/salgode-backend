ec010d60ab088fe071ad08a8a5248b20faf73b90
Franco Méndez <fnmendez@uc.cl>
2019-10-27 16:08:30 -0300

    fix(get_user_self): response

--- get_user_self.js
+++ get_user_self.js
@@ -3,15 +3,31 @@ const aws = require('aws-sdk');
 // eslint-disable-next-line import/no-absolute-path
 const bearerToUserId = require('/opt/nodejs/bearer_to_user_id.js');
 
+const UsersTableName = process.env.dynamodb_users_table_name;
+const VehiclesTableName = process.env.dynamodb_vehicles_table_name;
+
 const dynamoDB = new aws.DynamoDB.DocumentClient();
 
 async function getUser(userId) {
   const params = {
-    TableName: process.env.dynamodb_table_name,
+    TableName: UsersTableName,
     Key: {
       user_id: userId
     },
     ProjectionExpression: 'user_id, first_name, last_name, email, phone, user_identifications, vehicles'
+  };
+  const data = await dynamoDB.get(params).promise();
+  return data.Item;
+}
+
+async function getVehicle(vehicleId) {
+  const params = {
+    TableName: VehiclesTableName,
+    Key: {
+      vehicle_id: vehicleId
+    },
+    ProjectionExpression:
+      'vehicle_id, alias, vehicle_attributes, vehicle_identifications'
   };
   const data = await dynamoDB.get(params).promise();
   return data.Item;
@@ -22,6 +38,15 @@ exports.handler = async (event) => {
 
   const result = await getUser(userId);
   if (result) {
+    const myVehiclesRaw = await Promise.all(result.vehicles.map((v) => getVehicle(v)));
+    const myVehicles = myVehiclesRaw.map(
+      (v) => ({
+        vehicle_id: v.vehicle_id,
+        nickname: v.alias,
+        nickname_deprecation: 'Use alias instead',
+        alias: v.alias
+      })
+    );
     const response = {
       user_id: result.user_id,
       first_name: result.first_name,
@@ -32,12 +57,18 @@ exports.handler = async (event) => {
       user_verifications: {
         phone: !!result.phone,
         identity:
-          !!result.user_identifications.identification_image_front
-          && !!result.user_identifications.identification_image_back,
-        driver_license: !!result.user_identifications.driver_license
+          !!result.user_identifications.identification.front
+          && !!result.user_identifications.identification.back,
+        driver_license:
+          !!result.user_identifications.driver_license.front
+          && !!result.user_identifications.driver_license.back
       },
-      user_identifications: result.user_identifications,
-      vehicles: result.vehicles
+      user_identifications: {
+        selfie: result.user_identifications.selfie_image,
+        identification: result.user_identifications.identification,
+        driver_license: result.user_identifications.driver_license
+      },
+      vehicles: myVehicles
     };
     return {
       statusCode: 200,
