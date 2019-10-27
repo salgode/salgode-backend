da43047aeae995abc21223316ca16a30f150b779
Franco Méndez <fnmendez@uc.cl>
2019-10-27 20:39:26 -0300

    fix(update_user_vehicle): parameter processing

--- update_user_vehicle.js
+++ update_user_vehicle.js
@@ -1,20 +1,43 @@
 const aws = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
+const moment = require('moment');
+
+// eslint-disable-next-line import/no-absolute-path
+const bearerToUserId = require('/opt/nodejs/bearer_to_user_id.js');
+
+const EventsTableName = process.env.dynamodb_events_table_name;
+const VehiclesTableName = process.env.dynamodb_vehicles_table_name;
 
 const dynamoDB = new aws.DynamoDB.DocumentClient();
-const moment = require('moment');
 
 const updateableAttrs = [
   'alias',
-  'seats',
-  'vehicle_attributes'
+  'vehicle_attributes',
+  'vehicle_identifications'
 ];
 
-const VehicleTableName = process.env.dynamodb_vehicles_table_name;
+async function createEvent(userId, resourceId, resource, action, data) {
+  const eventId = `evt_${uuidv4()}`;
+  const timestamp = moment().format('YYYY-MM-DDTHH:mm:ss-04:00');
+  const params = {
+    TableName: EventsTableName,
+    Item: {
+      event_id: eventId,
+      user_id: userId,
+      resource_id: resourceId,
+      resource,
+      action,
+      event_data: data,
+      created_at: timestamp
+    }
+  };
+  await dynamoDB.put(params).promise();
+}
 
-function parseUpdateParams(body, updateableAttrs, updatedAt) {
+function parseUpdateParams(body, updatedAt) {
   let expression = 'SET ';
   const values = {};
-  for (let i = 0; i < updateableAttrs.length; i++) {
+  for (let i = 0; i < updateableAttrs.length; i += 1) {
     if (body[updateableAttrs[i]]) {
       expression += `${updateableAttrs[i]} = :${updateableAttrs[i]}, `;
       values[`:${updateableAttrs[i]}`] = body[updateableAttrs[i]];
@@ -33,12 +56,11 @@ async function updateUserVehicle(vehicleId, body) {
   const updatedAt = moment().format('YYYY-MM-DDTHH:mm:ss-04:00');
   const [updateExpression, expressionAttributeValues] = parseUpdateParams(
     body,
-    updateableAttrs,
     updatedAt
   );
 
   const params = {
-    TableName: VehicleTableName,
+    TableName: VehiclesTableName,
     Key: {
       vehicle_id: vehicleId
     },
@@ -50,13 +72,43 @@ async function updateUserVehicle(vehicleId, body) {
   return data;
 }
 
+async function getVehicle(vehicleId) {
+  const params = {
+    TableName: VehiclesTableName,
+    Key: {
+      vehicle_id: vehicleId
+    },
+    ProjectionExpression:
+      'vehicle_id, alias, vehicle_attributes, vehicle_identifications'
+  };
+  const data = await dynamoDB.get(params).promise();
+  return data.Item;
+}
+
 exports.handler = async (event) => {
+  const userId = await bearerToUserId.bearerToUserId(event.headers.Authorization.substring(7));
+
   const vehicleId = event.pathParameters.vehicle;
-  const body = JSON.parse(event.body);
+  let body = JSON.parse(event.body);
+
+  const vehicle = await getVehicle(vehicleId);
+  body = {
+    alias: body.alias || vehicle.alias,
+    vehicle_attributes: {
+      ...vehicle.vehicle_attributes, ...body.vehicle_attributes
+    },
+    vehicle_identifications: {
+      ...vehicle.vehicle_identifications, ...body.vehicle_identifications
+    }
+  };
   await updateUserVehicle(vehicleId, body);
+  await createEvent(userId, vehicleId, 'user', 'update', body);
 
   const message = {
-    action: 'updated', success: true, resource: 'vehicle', resource_id: vehicleId
+    action: 'update',
+    success: true,
+    resource: 'vehicle',
+    resource_id: vehicleId
   };
   const result = {
     body: JSON.stringify(message)
