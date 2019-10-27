eda2f442201a43adccd96e1de83095f87db82ea8
Franco Méndez <fnmendez@uc.cl>
2019-10-27 13:40:56 -0300

    fix(get_places): parse response

--- get_places.js
+++ get_places.js
@@ -5,17 +5,7 @@ const dynamoDB = new aws.DynamoDB.DocumentClient();
 async function getPlaces() {
   const params = {
     TableName: process.env.dynamodb_places_table_name,
-    ProjectionExpression: '#place_id, #name, #address, #commune, #city, #type, #lat, #lon',
-    ExpressionAttributeNames: {
-      '#place_id': 'place_id',
-      '#name': 'name',
-      '#address': 'address',
-      '#commune': 'commune',
-      '#city': 'city',
-      '#type': 'type',
-      '#lat': 'lat',
-      '#lon': 'lon'
-    }
+    ProjectionExpression: 'place_id, place_name, address, locality, city'
   };
   const data = await dynamoDB.scan(params).promise();
   return data.Items;
@@ -23,10 +13,21 @@ async function getPlaces() {
 
 exports.handler = async (event) => { // eslint-disable-line no-unused-vars
   const places = await getPlaces();
+  const responseBody = places.map((p) => ({
+    id: p.place_id,
+    id_deprecation: 'Use place_id instead',
+    place_id: p.place_id,
+    name: p.place_name,
+    name_deprecation: 'Use place_name instead',
+    place_name: p.place_name,
+    address: p.address,
+    locality: p.locality,
+    city: p.city
+  }));
   const response = {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(places)
+    body: JSON.stringify(responseBody)
   };
   return response;
 };
