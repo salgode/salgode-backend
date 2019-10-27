0c24a65f1d4acdde0f40b1b7b969bb1954ad5ffb
Franco Méndez <fnmendez@uc.cl>
2019-10-26 22:01:12 -0300

    refactor(get_user_current_trip): return empty object instead list

--- get_user_current_trip.js
+++ get_user_current_trip.js
@@ -187,6 +187,6 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify([])
+    body: JSON.stringify({})
   };
 };
