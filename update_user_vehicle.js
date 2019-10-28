84bd3ac7a01d1205dac0667f88f9fcb7b3d4d919
Franco Méndez <fnmendez@uc.cl>
2019-10-28 16:46:29 -0300

    refactor(update_user_vehicle): use transact for updating vehicle and creating event

--- update_user_vehicle.js
+++ update_user_vehicle.js
@@ -13,24 +13,6 @@ const updateableAttrs = [
   'vehicle_identifications'
 ];
 
-async function createEvent(userId, resourceId, resource, action, data) {
-  const eventId = `evt_${uuidv4()}`;
-  const timestamp = moment().format('YYYY-MM-DDTHH:mm:ss-04:00');
-  const params = {
-    TableName: EventsTableName,
-    Item: {
-      event_id: eventId,
-      user_id: userId,
-      resource_id: resourceId,
-      resource,
-      action,
-      event_data: data,
-      created_at: timestamp
-    }
-  };
-  await dynamoDB.put(params).promise();
-}
-
 function parseUpdateParams(body, updatedAt) {
   let expression = 'SET ';
   const values = {};
@@ -41,32 +23,12 @@ function parseUpdateParams(body, updatedAt) {
     }
   }
   if (Object.keys(values).length) {
-    expression += 'updatedAt = :updatedAt';
+    expression += 'updated_at = :updatedAt';
     values[':updatedAt'] = updatedAt;
   } else {
     throw new Error('[BadRequest] No updateable attributes specified');
   }
   return [expression, values];
-}
-
-async function updateUserVehicle(vehicleId, body) {
-  const updatedAt = moment().format('YYYY-MM-DDTHH:mm:ss-04:00');
-  const [updateExpression, expressionAttributeValues] = parseUpdateParams(
-    body,
-    updatedAt
-  );
-
-  const params = {
-    TableName: VehiclesTableName,
-    Key: {
-      vehicle_id: vehicleId
-    },
-    UpdateExpression: updateExpression,
-    ExpressionAttributeValues: expressionAttributeValues,
-    ReturnValues: 'NONE'
-  };
-  const data = await dynamoDB.update(params).promise();
-  return data;
 }
 
 async function getVehicle(vehicleId) {
@@ -82,14 +44,58 @@ async function getVehicle(vehicleId) {
   return data.Item;
 }
 
+async function updateVehicle(userId, vehicleId, updateParams, rawData) {
+  const eventId = `evt_${uuidv4()}`;
+  const timestamp = moment().format('YYYY-MM-DDTHH:mm:ss-04:00');
+  const [updateExpression, expressionAttributeValues] = parseUpdateParams(
+    updateParams,
+    timestamp
+  );
+  try {
+    await dynamoDB
+      .transactWrite({
+        TransactItems: [
+          {
+            Update: {
+              TableName: VehiclesTableName,
+              Key: {
+                vehicle_id: vehicleId
+              },
+              UpdateExpression: updateExpression,
+              ExpressionAttributeValues: expressionAttributeValues
+            }
+          },
+          {
+            Put: {
+              TableName: EventsTableName,
+              Item: {
+                event_id: eventId,
+                user_id: userId,
+                resource_id: vehicleId,
+                resource: 'vehicle',
+                action: 'update',
+                event_data: rawData,
+                created_at: timestamp
+              }
+            }
+          }
+        ]
+      })
+      .promise();
+    return vehicleId;
+  } catch (e) {
+    return false;
+  }
+}
+
 exports.handler = async (event) => {
   const userId = event.requestContext.authorizer.user_id;
 
   const vehicleId = event.pathParameters.vehicle;
-  let body = JSON.parse(event.body);
+  const body = JSON.parse(event.body);
 
   const vehicle = await getVehicle(vehicleId);
-  body = {
+  const updateParams = {
     alias: body.alias || vehicle.alias,
     vehicle_attributes: {
       ...vehicle.vehicle_attributes, ...body.vehicle_attributes
@@ -98,18 +104,29 @@ exports.handler = async (event) => {
       ...vehicle.vehicle_identifications, ...body.vehicle_identifications
     }
   };
-  await updateUserVehicle(vehicleId, body);
-  await createEvent(userId, vehicleId, 'vehicle', 'update', body);
-
-  const message = {
-    action: 'update',
-    success: true,
-    resource: 'vehicle',
-    resource_id: vehicleId
+  const result = await updateVehicle(userId, vehicleId, updateParams, body);
+  if (result) {
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(
+        {
+          action: 'update',
+          success: true,
+          resource: 'vehicle',
+          resource_id: vehicleId
+        }
+      )
+    };
+  }
+  return {
+    statusCode: 400,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify({
+      action: 'update',
+      success: false,
+      resource: 'vehicle',
+      message: 'Request contains errors'
+    })
   };
-  const result = {
-    body: JSON.stringify(message)
-  };
-
-  return result;
 };
