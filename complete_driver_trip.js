239b266f07def5a7be70e4bc6420ec1439b297f5
Franco Méndez <fnmendez@uc.cl>
2019-11-04 01:33:25 -0300

    feat(complete_driver_trip): do not increase current point and handle error response

--- complete_driver_trip.js
+++ complete_driver_trip.js
@@ -7,34 +7,43 @@ const dynamoDB = new aws.DynamoDB.DocumentClient();
 
 async function completeTrip(tripId, userId) {
   const timestamp = moment().format('YYYY-MM-DDTHH:mm:ss-04:00');
-  const expectedStatus = 'in_progress';
   const params = {
     TableName: TripsTableName,
     Key: {
       trip_id: tripId
     },
     UpdateExpression:
-      'set current_point = current_point + :val, trip_status = :newStatus, updated_at = :now',
+      'set trip_status = :newStatus, updated_at = :now',
     ConditionExpression:
       'trip_status = :expectedStatus and driver_id = :self',
     ExpressionAttributeValues: {
-      ':val': 1,
       ':newStatus': 'completed',
       ':now': timestamp,
       ':self': userId,
-      ':expectedStatus': expectedStatus
-    },
-    ReturnValues: 'UPDATED_NEW'
+      ':expectedStatus': 'in_progress'
+    }
   };
-  const data = await dynamoDB.update(params).promise();
-  return data.Attributes;
+  return dynamoDB.update(params).promise();
 }
 
 exports.handler = async (event) => {
   const userId = event.requestContext.authorizer.user_id;
   const tripId = event.pathParameters.trip;
 
-  await completeTrip(tripId, userId);
+  try {
+    await completeTrip(tripId, userId);
+  } catch (err) {
+    return {
+      statusCode: 409,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        action: 'complete',
+        success: false,
+        resource: 'trip',
+        resource_id: tripId
+      })
+    };
+  }
   const responseBody = {
     action: 'complete',
     success: true,
