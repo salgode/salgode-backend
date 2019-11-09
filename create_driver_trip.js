4424fb86bade8cf6bd24751429006dc4f31c5d87
Franco Méndez <fnmendez@uc.cl>
2019-11-09 20:01:47 -0300

    feat(create_driver_trip): add robustness against unexistent resources

--- create_driver_trip.js
+++ create_driver_trip.js
@@ -2,13 +2,25 @@ const aws = require('aws-sdk');
 const uuidv4 = require('uuid/v4');
 const moment = require('moment');
 
+const PlacesTableName = process.env.dynamodb_places_table_name;
 const TripsTableName = process.env.dynamodb_trips_table_name;
 const UsersTableName = process.env.dynamodb_users_table_name;
+const VehiclesTableName = process.env.dynamodb_vehicles_table_name;
 
 const dynamoDB = new aws.DynamoDB.DocumentClient();
 
 function isEmpty(obj) {
   return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
+
+function mapIdKeys(ids, key) {
+  return ids.map((i) => ({
+    [key]: i
+  }));
+}
+
+function repeated(value, index, self) {
+  return self.indexOf(value) === index;
 }
 
 async function createTrip(driverId, vehicleId, availableSeats, etdInfo, routePoints) {
@@ -30,7 +42,7 @@ async function createTrip(driverId, vehicleId, availableSeats, etdInfo, routePoi
     }
   };
   await dynamoDB.put(params).promise();
-  return [tripId, timestamp];
+  return tripId;
 }
 
 function alreadyPassed(time) {
@@ -49,6 +61,29 @@ async function getUser(userId) {
   return data.Item;
 }
 
+async function getVehicle(vehicleId) {
+  const params = {
+    TableName: VehiclesTableName,
+    Key: {
+      vehicle_id: vehicleId
+    }
+  };
+  const data = await dynamoDB.get(params).promise();
+  return data.Item;
+}
+
+async function getPlaces(placeIds) {
+  const params = {
+    RequestItems: {
+      [PlacesTableName]: {
+        Keys: mapIdKeys(placeIds, 'place_id')
+      }
+    }
+  };
+  const data = await dynamoDB.batchGet(params).promise();
+  return data.Responses[PlacesTableName];
+}
+
 exports.handler = async (event) => {
   const userId = event.requestContext.authorizer.user_id;
   const body = JSON.parse(event.body);
@@ -61,6 +96,26 @@ exports.handler = async (event) => {
     !etdInfo || isEmpty(etdInfo) || !etdInfo.etd || alreadyPassed(etdInfo.etd)
     || !routePoints || !(routePoints.length > 0) || !vehicleId || !(availableSeats > 0)
   ) {
+    return {
+      statusCode: 400,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        action: 'create',
+        success: false,
+        resource: 'trip',
+        message: 'Wrong or missing parameters'
+      })
+    };
+  }
+
+  try {
+    const points = routePoints.filter(repeated);
+    const vehicle = await getVehicle(vehicleId);
+    const places = await getPlaces(points);
+    if (!vehicle || places.length !== points.length) {
+      throw new Error('Unexistent resource');
+    }
+  } catch (err) {
     return {
       statusCode: 400,
       headers: { 'Access-Control-Allow-Origin': '*' },
@@ -90,7 +145,7 @@ exports.handler = async (event) => {
     };
   }
 
-  const [tripId, timestamp] = await createTrip(
+  const tripId = await createTrip(
     userId, vehicleId, availableSeats, etdInfo, routePoints
   );
 
@@ -101,8 +156,7 @@ exports.handler = async (event) => {
       action: 'create',
       success: true,
       resource: 'trip',
-      resource_id: tripId,
-      performed_at: timestamp
+      resource_id: tripId
     })
   };
 };
