9ad095c555a5f9dbdff21a5e1210fd21a1b19949
Franco Méndez <fnmendez@uc.cl>
2019-11-19 15:37:54 -0300

    feat(get_passenger_reservation): add trip route info

--- get_passenger_reservation.js
+++ get_passenger_reservation.js
@@ -1,12 +1,15 @@
 const aws = require('aws-sdk');
 
+const ImagesBaseUrl = process.env.salgode_images_bucket_base_url;
 const PlacesTableName = process.env.dynamodb_places_table_name;
+let ImagesTableName = process.env.dynamodb_images_table_name;
 let ReservationsTableName = process.env.dynamodb_reservations_table_name;
 let TripsTableName = process.env.dynamodb_trips_table_name;
 let UsersTableName = process.env.dynamodb_users_table_name;
 let VehiclesTableName = process.env.dynamodb_vehicles_table_name;
 
 function stagingOverwrite() {
+  ImagesTableName = `Dev_${process.env.dynamodb_images_table_name}`;
   ReservationsTableName = `Dev_${process.env.dynamodb_reservations_table_name}`;
   TripsTableName = `Dev_${process.env.dynamodb_trips_table_name}`;
   UsersTableName = `Dev_${process.env.dynamodb_users_table_name}`;
@@ -14,6 +17,16 @@ function stagingOverwrite() {
 }
 
 const dynamoDB = new aws.DynamoDB.DocumentClient();
+
+function mapIdKeys(ids, key) {
+  return ids.map((i) => ({
+    [key]: i
+  }));
+}
+
+function parseUrl(baseUrl, folder, file) {
+  return `${baseUrl}/${folder}/${file}`;
+}
 
 async function getReservation(reservationId) {
   const params = {
@@ -41,6 +54,19 @@ async function getTrip(tripId) {
   return data.Item;
 }
 
+async function getImageUrl(imageId) {
+  const params = {
+    TableName: ImagesTableName,
+    Key: {
+      image_id: imageId
+    },
+    ProjectionExpression: 'file_name, folder_name'
+  };
+  const data = await dynamoDB.get(params).promise();
+  const image = data.Item;
+  return parseUrl(ImagesBaseUrl, image.folder_name, image.file_name);
+}
+
 async function getDriverInformation(driverId) {
   const params = {
     TableName: UsersTableName,
@@ -55,7 +81,7 @@ async function getDriverInformation(driverId) {
     driver_id: driverId,
     driver_name: data.Item.first_name,
     driver_phone: data.Item.phone,
-    driver_avatar: data.Item.user_identifications.selfie_image,
+    driver_avatar: await getImageUrl(data.Item.user_identifications.selfie_image),
     driver_verifications: {
       email: data.Item.user_verifications.email,
       phone: data.Item.user_verifications.phone,
@@ -90,13 +116,13 @@ async function getVehicleInformation(vehicleId) {
   return response;
 }
 
-async function getFullPlaceInfoFromReservationRoute(routePlaces) {
+async function getFullPlaceInfoFromRoute(placeIds) {
   const params = {
     RequestItems: {
       [PlacesTableName]: {
-        Keys: [{ place_id: routePlaces.start }, { place_id: routePlaces.end }],
+        Keys: mapIdKeys(placeIds, 'place_id'),
         ProjectionExpression:
-          'place_id, place_name'
+          'place_id, place_name, lat, lon'
       }
     }
   };
@@ -105,7 +131,34 @@ async function getFullPlaceInfoFromReservationRoute(routePlaces) {
 }
 
 async function formatResponse(reservation, trip) {
-  const tripRoute = await getFullPlaceInfoFromReservationRoute(reservation.route);
+  let place;
+  const reservationRouteRaw = await getFullPlaceInfoFromRoute(Object.values(reservation.route));
+  const tripRouteRaw = await getFullPlaceInfoFromRoute(trip.route_points);
+  const reservationRoute = [];
+  const tripRoute = [];
+  const startPlace = reservationRouteRaw.find((rr) => rr.place_id === reservation.route.start);
+  const endPlace = reservationRouteRaw.find((rr) => rr.place_id === reservation.route.end);
+  reservationRoute.push({
+    place_id: startPlace.place_id,
+    place_name: startPlace.place_name,
+    lat: startPlace.lat,
+    lon: startPlace.lon
+  });
+  reservationRoute.push({
+    place_id: endPlace.place_id,
+    place_name: endPlace.place_name,
+    lat: endPlace.lat,
+    lon: endPlace.lon
+  });
+  for (let i = 0; i < tripRouteRaw.length; i += 1) {
+    place = tripRouteRaw.find((rr) => rr.place_id === trip.route_points[i]);
+    tripRoute.push({
+      place_id: place.place_id,
+      place_name: place.place_name,
+      lat: place.lat,
+      lon: place.lon
+    });
+  }
   return {
     reservation_id: reservation.reservation_id,
     reservation_status: reservation.reservation_status,
@@ -114,11 +167,15 @@ async function formatResponse(reservation, trip) {
     driver: await getDriverInformation(trip.driver_id),
     vehicle: await getVehicleInformation(trip.vehicle_id),
     etd_info: trip.etd_info,
-    route: trip.route,
+    reservation_route: {
+      start: reservationRoute[0],
+      end: reservationRoute[1]
+    },
     trip_route: {
-      start: tripRoute.find((p) => p.place_id === reservation.route.start),
-      end: tripRoute.find((p) => p.place_id === reservation.route.start)
-    }
+      start: tripRoute[0],
+      end: tripRoute[tripRoute.length - 1]
+    },
+    trip_route_points: tripRoute
   };
 }
 
